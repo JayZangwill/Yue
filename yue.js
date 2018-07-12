eddb415a8d54e6134e3f29dae7c19609d5d48a1b
Jay Zangwill <359390851@qq.com>
2018-07-12 19:35:42 +0800

    学习

--- yue.js
+++ yue.js
@@ -31,12 +31,22 @@
       new Observer(data);
     }
   }
+  var methodKeys = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
+  // var arrayMethods = methodKeys.map(function(){
+  //   return 
+  // })
+  //需添加arrayMethods
 
   function Observer(data) {
     this.data = data;
     defaultProperty(data, '__ob__', this);
     if (!Array.isArray(data)) {
       this.walk(data);
+    } else {
+      // 将变异方法覆盖到数据上
+      argument(data, arrayMethods, methodKeys);
+      // 深度观察数据
+      this.observeArray(data);
     }
   }
 
@@ -46,6 +56,18 @@
       defineReactive(data, keys[i]);
     }
   };
+
+  Observer.prototype.observeArray = function (data) {
+    for (var i = 0; i < data.length; i++) {
+      observe(data[i]);
+    }
+  }
+
+  function argument(target, arrayMethods, keys) {
+    for (var i = 0; i < keys.length; i++) {
+      defaultProperty(target, keys[i], arrayMethods[keys[i]])
+    }
+  }
 
   function defaultProperty(target, key, value) {
     Object.defineProperty(target, key, {
